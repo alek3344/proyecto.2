1424a7a47768ad64c131a2acc375eac00e45d9ab
alek3344 <104906366+alek3344@users.noreply.github.com>
2022-06-04 11:39:42 -0500

    Add files via upload

---
+++ proyecto1/src/modulo/Oficina.java
@@ -0,0 +1,13 @@
+
+package modulo;
+
+public abstract class Oficina {
+    
+    
+    public abstract void setCasos(int n);
+    public abstract void detectivesActivos();
+    public abstract void setnewcaso(int n);
+    public abstract void delete(int n);
+    public abstract void mostrarCasos(int n);
+    
+}
